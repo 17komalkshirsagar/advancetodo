4c776e5610a90294d5254fcb10762bec6f1e372e
komalmaticui <komal@maticui.com>
2024-08-28 11:19:40 +0530

    controller

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(cookieParser())
 //routes
 
-app.use("/api/auth", require("./routes/routes.auth.routes"))
+app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/admin", require("./routes/admin.routes"))
 app.use("*", (req, res) => {
     res.status(404).json({ message: "Resource Not Found" })
